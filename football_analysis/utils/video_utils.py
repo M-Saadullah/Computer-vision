ff5967a1a79a99be2941515e06b2a44d363d0a24
M-Saadullah <saadullahm037@gmail.com>
2025-02-06 00:41:52 +0500

    project

---
+++ football_analysis/utils/video_utils.py
@@ -0,0 +1,23 @@
+import cv2
+import os
+import time
+def read_video(video_path):
+    cap = cv2.VideoCapture(video_path)
+    frames = []
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frames.append(frame)
+    return frames
+
+def save_video(ouput_video_frames,output_video_path):
+    base_name, extension = os.path.splitext(output_video_path)
+    
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+    output_video_path = f"{base_name}_{timestamp}{extension}"
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(output_video_path, fourcc, 24, (ouput_video_frames[0].shape[1], ouput_video_frames[0].shape[0]))
+    for frame in ouput_video_frames:
+        out.write(frame)
+    out.release()
